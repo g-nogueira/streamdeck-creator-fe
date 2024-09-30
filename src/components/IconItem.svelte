1831f646510b74ad494ca17bbb42276062fe30e6
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-09-30 22:05:49 +0100

    refactor: add IconItem component

---
+++ src/components/IconItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Icon } from '../stores';
+
+  export let icon: Icon;
+  export let onClick: (icon: Icon) => void;
+</script>
+
+<div class="flex col-span-1" on:click={() => onClick(icon)}>
+  <div class="flex flex-col mx-auto h-40 w-40 border border-gray-300 hover:bg-gray-200 transition-all cursor-pointer">
+    <div class="relative pt-4 h-24 w-24 mx-auto">
+      <img src={`http://localhost:5199/icons/${icon.id}`} alt={icon.label} class="absolute inset-0 h-full w-full" />
+    </div>
+    <div class="flex w-full py-4 px-2 text-center">
+      <span class="mx-auto font-semibold text-sm truncate">{icon.label}</span>
+    </div>
+  </div>
+</div>
--- src/components/IconList.svelte
+++ src/components/IconList.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import { icons, selectedIcon } from '../stores';
   import type { Icon } from '../stores';
+  import IconItem from './IconItem.svelte';
 
   export let classNames: string = '';
 
@@ -19,16 +20,7 @@
 <div class="h-full flex-grow w-0 flex flex-col">
   <div class={`py-8 px-4 grid grid-cols-4 gap-y-8 ${classNames}`}>
     {#each $icons as icon}
-      <div class="flex col-span-1" on:click={() => selectIcon(icon)}>
-        <div class="flex flex-col mx-auto h-40 w-40 border border-gray-300 hover:bg-gray-200 transition-all cursor-pointer">
-          <div class="relative pt-4 h-24 w-24 mx-auto">
-            <img src={`http://localhost:5199/icons/${icon.id}`} alt={icon.label} class="absolute inset-0 h-full w-full" />
-          </div>
-          <div class="flex w-full py-4 px-2 text-center">
-            <span class="mx-auto font-semibold text-sm truncate">{icon.label}</span>
-          </div>
-        </div>
-      </div>
+      <IconItem icon={icon} onClick={selectIcon} />
     {/each}
   </div>
 </div>
