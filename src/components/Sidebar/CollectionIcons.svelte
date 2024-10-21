09e2ac997ddc32d39f8735bd2da38ead3b24022d
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-10-21 20:34:33 +0100

    feat: add delete UserIcon functionality (#2)

--- src/components/Sidebar/CollectionIcons.svelte
+++ src/components/Sidebar/CollectionIcons.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import * as _selectedIcon from '../../models/SelectedIcon';
+	import RemoveFromCollectionIcon from "lucide-svelte/icons/x";
 	import type { UserIcon } from '../../models/UserIcon';
 	import type { UserIconCollection } from '../../models/UserIconCollection';
 	import { selectedIcon } from '../../stores/selected-icon.store';
+	import {userIconCollections} from '../../stores/user-icon-collection.store';
+
 
 	let { collection } : { collection : UserIconCollection} = $props()
 
@@ -20,15 +23,30 @@
 		selectedIcon.selectIcon(newSelectedIcon);
 	}
 
+	function removeIcon(icon: UserIcon, collection: UserIconCollection) {
+		userIconCollections.removeIcon(collection.id, icon.id);
+	}
+
 </script>
 
 <div>
 	<div class="h-auto w-full flex flex-row flex-wrap gap-3">
 		{#each collection.icons as icon}
-			<button type="button" class="h-fit w-24 flex flex-col items-center gap-3 p-2 hover:bg-surface-800 rounded-md transition-all cursor-pointer" onclick={() => selectUserIcon(icon, collection)} aria-label={`Select icon ${icon.label}`} >
-				<img src={icon.pngData} alt={icon.label} class="h-15 w-15" />
-				<span class="w-full font-semibold text-sm truncate">{icon.label}</span>
-			</button>
+			<div class="relative icon-container">
+				<button id="removeBtn" type="button" onclick={() => removeIcon(icon, collection)} aria-label={`Remove icon ${icon.label}`} class="hidden badge-icon absolute -right-0 -top-0 z-10 bg-error-900 hover:bg-error-950">
+					<RemoveFromCollectionIcon size={20} />
+				</button>
+				<button type="button" class="h-fit w-24 flex flex-col items-center gap-3 p-2 rounded-md transition-all cursor-pointer" onclick={() => selectUserIcon(icon, collection)} aria-label={`Select icon ${icon.label}`} >
+					<img src={icon.pngData} alt={icon.label} class="h-15 w-15" />
+					<span class="w-full font-semibold text-sm truncate">{icon.label}</span>
+				</button>
+			</div>
 		{/each}
 	</div>
 </div>
+<style lang="postcss">
+	.icon-container:hover #removeBtn {
+		display: flex;
+	}
+</style>
+
