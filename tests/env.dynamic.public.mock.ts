44bb270786728c9f4c3f4f750b2ede9955fbfd5b
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-11-17 21:33:08 +0000

    feat: add mock implementations for env.public and icon.store

---
+++ tests/env.dynamic.public.mock.ts
@@ -0,0 +1,7 @@
+// Mock for the `env.public` module.
+export const env = {
+    PUBLIC_SERVICE_URL: 'https://example.com',
+    PUBLIC_UNLEASH_URL: "http://localhost:4242",
+    PUBLIC_UNLEASH_API_TOKEN: "default:development.unleash-insecure-frontend-api-token",
+    PUBLIC_UNLEASH_APP_NAME: "default"
+};
