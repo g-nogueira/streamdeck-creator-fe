af326223e25d8f793604d76e7eb095ea6f3df4c4
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2025-03-06 18:09:51 +0000

    refactor: clean up icon customizations store by removing commented code and adding state checks

--- src/stores/icon-customizations.store.ts
+++ src/stores/icon-customizations.store.ts
@@ -6,35 +6,6 @@ import * as _iconPreview from '../models/IconPreview';
 import _ from "lodash";
 import type { GradientStop, IconGradient } from "../models/IconGradient";
 
-
-	// $: if ($customizedIcon) {
-	// 	// Has userIconId and isn't empty uuid?
-	// 	if ($customizedIcon.userIconId && $customizedIcon.userIconId !== UUID.empty) {
-	// 		if (isContentTypeSvg($customizedIcon.contentType) && $customizedIcon.svgContent) {
-	// 			customizedIcon.selectSvgIcon($customizedIcon.svgContent);
-	// 		} else {
-	// 			// _.flow(IconService.mkIconUrl, customizedIcon.selectImageIcon)($customizedIcon.originalIconId);
-	// 			throw new Error('User icon is not an SVG. Only SVGs are supported for now.');
-	// 		}
-	// 	}
-	// 	// Has iconId and userIconId is empty uuid?
-	// 	// Then keeps the state.styles as it is, so that the user can reuse the previous styles
-	// 	else if ($customizedIcon.iconId) {
-	// 		IconService.fetchIconWithContentType($customizedIcon.iconId, $customizedIcon.iconOrigin)
-	// 			.then(([iconContent, contentType]) => {
-	// 				if (isContentTypeSvg(contentType)) {
-	// 					customizedIcon.selectSvgIcon(iconContent);
-	// 				} else {
-	// 					_.flow(IconService.mkIconUrl, customizedIcon.selectImageIcon)($customizedIcon.iconId);
-	// 				}
-	// 			})
-	// 			.catch((error) => {
-	// 				throw new Error('Error fetching icon', error);
-	// 			});
-	// 	}
-	// }
-
-
 function createIconCustomizationsStore() {
     const { subscribe, set, update } = writable<IconPreview | null>(null);
 
@@ -66,18 +37,18 @@ function createIconCustomizationsStore() {
         const parser = new DOMParser();
         const doc = parser.parseFromString(svgContent, 'image/svg+xml');
         const svgElement = doc.querySelectorAll('svg');
-    
+
         _.forEach(svgElement, (element) => {
             element.setAttribute('fill', color);
         });
-    
+
         return new XMLSerializer().serializeToString(doc);
     }
-    
+
     const removeSvgSizeAttributes = (svgContent: string): string => {
         return svgContent.replace(/(width|height)="[^"]*"/g, '');
     }
-    
+
     function mkDefaultGradient(): IconGradient {
         return {
             stops: [
@@ -89,12 +60,12 @@ function createIconCustomizationsStore() {
             cssStyle: 'linear-gradient(90deg, #fc466b 0%, #3f5efb 100%)'
         };
     }
-    
+
     function mkCssStyle({ stops, type, angle }: IconGradient): string {
         if (!type) {
             return 'linear-gradient(90deg, #45fc8b 0%, #212a54 100%)';
         }
-    
+
         return type === 'linear' ? `linear-gradient(${angle}deg, ${stops.map(s => `${s.color} ${s.position}%`).join(', ')})` : 'radial-gradient(circle, ' + stops.map(s => `${s.color} ${s.position}%`).join(', ') + ')';
     }
 
@@ -105,52 +76,67 @@ function createIconCustomizationsStore() {
         clear: () => set(null),
 
         selectSvgIcon: (svg: string) => update((state) => {
-            const cleanedSvg = _.flow(updateSvgFill(state!.styles.glyphColor), removeSvgSizeAttributes)(svg);
+            if (!state) return state;
 
-            state!.svgContent = cleanedSvg;
-            state!.imageUrl = '';
-            state!.styles.pngData = '';
+            const cleanedSvg = _.flow(updateSvgFill(state.styles.glyphColor), removeSvgSizeAttributes)(svg);
+
+            state.svgContent = cleanedSvg;
+            state.imageUrl = '';
+            state.styles.pngData = '';
 
             return state;
         }),
 
         selectImageIcon: (url: string) => update(state => {
-            state!.imageUrl = url;
-            state!.svgContent = '';
+            if (!state) return state;
+
+            state.imageUrl = url;
+            state.svgContent = '';
 
             return state;
         }),
 
         setIconTitle: (title: string) => update(state => {
-            state!.styles.label = title;
+            if (!state) return state;
+
+            state.styles.label = title;
             return state;
         }),
 
         upsertStyles: (styles: Partial<IconPreview['styles']>) => update(state => {
-            state!.styles = { ...state!.styles, ...styles };
+            if (!state) return state;
+
+            state.styles = { ...state.styles, ...styles };
             return state;
         }),
 
         setSvgFillColor: (color: string) => update(state => {
-            state!.svgContent = updateSvgFill(color)(state!.svgContent!);
+            if (!state) return state;
+
+            state.svgContent = updateSvgFill(color)(state.svgContent!);
             return state;
         }),
 
         setUseGradient: (value: boolean) => update(state => {
-            state!.styles.useGradient = value;
+            if (!state) return state;
+
+            state.styles.useGradient = value;
             return state;
         }),
 
         upsertGradient: (gradient: Partial<IconGradient>) => {
-            const uiState = get({subscribe});
-            const oldGradient : any = _.cloneDeep(uiState!.styles.gradient || {});
+            const uiState = get({ subscribe });
 
-            const newUiState = ((state : IconPreview) => {
+            if (!uiState || !uiState.styles) throw new Error('IconPreview state is not initialized yet.');
+
+            const oldGradient: any = _.cloneDeep(uiState.styles.gradient || {});
+
+            const newUiState = ((state: IconPreview) => {
                 state.styles.gradient ||= mkDefaultGradient();
                 state.styles.gradient = { ...state.styles.gradient, ...gradient };
                 state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
                 return state;
-            })(uiState!);
+            })(uiState);
 
             // Checkes if any prop has changed
             if (_.isEqual(oldGradient, newUiState.styles.gradient)) return;
@@ -159,50 +145,64 @@ function createIconCustomizationsStore() {
         },
 
         addGradientStop: (stop: GradientStop) => update(state => {
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.stops = [...state.styles.gradient.stops, stop].sort((a, b) => a.position - b.position);
-            state!.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
+            if (!state) return state;
+
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.stops = [...state.styles.gradient.stops, stop].sort((a, b) => a.position - b.position);
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         updateGradientStopPosition: (index: number, position: GradientStop['position']) => update(state => {
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.stops[index].position = position;
-            state!.styles.gradient.stops = state!.styles.gradient.stops.sort((a, b) => a.position - b.position);
-            state!.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
+            if (!state) return state;
+
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.stops[index].position = position;
+            state.styles.gradient.stops = state.styles.gradient.stops.sort((a, b) => a.position - b.position);
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         updateGradientStopColor: (index: number, color: GradientStop['color']) => update(state => {
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.stops[index].color = color;
-            state!.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
+            if (!state) return state;
+
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.stops[index].color = color;
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         removeGradientStop: (index: number) => update(state => {
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.stops = state!.styles.gradient.stops.filter((_, i) => i !== index);
-            state!.styles.gradient.cssStyle = mkCssStyle(state!.styles.gradient);
+            if (!state) return state;
+
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.stops = state.styles.gradient.stops.filter((_, i) => i !== index);
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         setGradientType: (type: IconGradient['type']) => update(state => {
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.type = type;
-            state!.styles.gradient.cssStyle = mkCssStyle(state!.styles.gradient);
+            if (!state) return state;
+
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.type = type;
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         recalculateGradientCss: () => update(state => {
+            if (!state) return state;
+
             console.log('Recalculating gradient css');
-            state!.styles.gradient ||= mkDefaultGradient();
-            state!.styles.gradient.cssStyle = mkCssStyle(state!.styles.gradient);
+            state.styles.gradient ||= mkDefaultGradient();
+            state.styles.gradient.cssStyle = mkCssStyle(state.styles.gradient);
             return state;
         }),
 
         updatePngData: (data: string) => update(state => {
-            state!.styles.pngData = data;
+            if (!state) return state;
+
+            state.styles.pngData = data;
             return state;
         }),
 
