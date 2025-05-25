9f0c4329f08e4e669a608da504f5b3c528a05ce0
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2025-05-25 02:59:53 +0100

    fix: update icon rendering logic for homarr origin

--- src/components/icons/IconPreview.svelte
+++ src/components/icons/IconPreview.svelte
@@ -24,14 +24,12 @@
 				data-testid="icon-background">
 				<!-- Icon -->
 				<div
-					data-testid="icon-wrapper"
-					class="flex max-h-[223px] w-full flex-grow justify-center p-5"
-					style="color: {customizableIcon.styles.glyphColor}; transform: scale({customizableIcon.styles
-						.iconScale}) translate({customizableIcon.styles.imgX}px, {customizableIcon.styles.imgY}px);">
-					{#if customizableIcon.contentType === "image/svg+xml" && customizableIcon.svgContent}
+				data-testid="icon-wrapper"
+				class="h-[223px] w-full p-5 flex" style="color: {customizableIcon.styles.glyphColor}; transform: scale({customizableIcon.styles.iconScale}) translate({customizableIcon.styles.imgX}px, {customizableIcon.styles.imgY}px);">
+					{#if customizableIcon.contentType === "image/svg+xml" && customizableIcon.svgContent && customizableIcon.iconOrigin !== "homarr"}
 						{@html customizableIcon.svgContent}
-					{:else if customizableIcon.contentType === "image/png"}
-						<img src={customizableIcon.imageIconUrl} alt="icon preview" style="max-width:100%;max-height:100%;" />
+					{:else if customizableIcon.iconOrigin === "homarr"}
+						<img src={customizableIcon.imageIconUrl} alt="icon preview" class="h-full m-auto max-h-full" style="width: 100%;height: auto;object-fit: contain;" />
 					{/if}
 				</div>
 				<!-- Label -->
